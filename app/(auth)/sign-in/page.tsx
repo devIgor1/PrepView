db8240ca450309a7f6244803f8c9baac413e62d5
devIgor1 <igormraesmm@gmail.com>
2025-06-28 18:40:23 -0300

    feat: changing layouts between pages using props

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,7 @@
+import AuthForm from "@/components/AuthForm"
+
 const Page = () => {
-  return (
-    <div>
-      <h1>Sign In</h1>
-    </div>
-  )
+  return <AuthForm type="sign-in" />
 }
 
 export default Page
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,7 @@
+import AuthForm from "@/components/AuthForm"
+
 const Page = () => {
-  return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
-  )
+  return <AuthForm type="sign-up" />
 }
 
 export default Page
