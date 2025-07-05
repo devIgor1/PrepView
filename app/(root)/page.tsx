234fd06dd611cbb130a00dd46b0cfe5e8cd0416f
devIgor1 <igormraesmm@gmail.com>
2025-07-05 12:24:17 -0300

    feat: add dayjs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,31 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+
 const Page = () => {
-  return <div>Home Page</div>
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice with real interview questions, get instant feedback, and
+            track your progress.
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Get Started</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+    </>
+  )
 }
 
 export default Page
