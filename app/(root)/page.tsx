9f300c4251a37727efc4681990993301394f1238
devIgor1 <igormraesmm@gmail.com>
2025-07-05 13:43:54 -0300

    feat: start using mock object interview

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from "@/components/InterviewCard"
 import { Button } from "@/components/ui/button"
+import { dummyInterviews } from "@/public/constants"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -23,6 +25,26 @@ const Page = () => {
           height={400}
           className="max-sm:hidden"
         />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.length === 0 ? (
+            <p>You haven&apos;t taken any interviews yet.</p>
+          ) : (
+            <div className="flex flex-col gap-6">
+              {dummyInterviews.map((interview) => (
+                <InterviewCard key={interview.id} {...interview} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          <p>There are no interviews available at the moment.</p>
+        </div>
       </section>
     </>
   )
